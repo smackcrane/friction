3e76a5dadfa3346fa4bd8f93141bf5224e262a37
Sander Mack-Crane <smackcrane@gmail.com>
2025-07-12 17:25:54 -0600

    eat yt sidebar recommendations and endscreen recommendations

--- feed-eater.js
+++ feed-eater.js
@@ -3,6 +3,10 @@ function feedEater() {
     const elementsToEat = [
         'ytd-browse[page-subtype="home"]', // Home feed
         'ytd-shorts', // Shorts feed
+        'ytd-watch-next-secondary-results-renderer', // Watch next feed
+        // 'div#secondary.ytd-watch-flexy', // Watch next feed, alternative selector
+        'div.videowall-endscreen', // End screen recommendations
+        // 'div.html5-endscreen', // End screen recommendations, alternative selector
     ]
     for (const selector of elementsToEat) {
         const elements = document.querySelectorAll(selector)
