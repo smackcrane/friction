90aa3d50378e9959bbc40ac94847f05f951998bf
Sander Mack-Crane <smackcrane@gmail.com>
2025-07-12 18:50:36 -0600

    format with prettier

--- feed-eater.js
+++ feed-eater.js
@@ -1,23 +1,22 @@
-
 function feedEater() {
-    const elementsToEat = [
-        'ytd-browse[page-subtype="home"]', // Home feed
-        'ytd-shorts', // Shorts feed
-        'ytd-watch-next-secondary-results-renderer', // Watch next feed
-        // 'div#secondary.ytd-watch-flexy', // Watch next feed, alternative selector
-        'div.videowall-endscreen', // End screen recommendations
-        // 'div.html5-endscreen', // End screen recommendations, alternative selector
-    ]
-    for (const selector of elementsToEat) {
-        const elements = document.querySelectorAll(selector)
-        elements.forEach(element => {
-            element.remove()
-            console.log(`${selector} removed`)
-        })
-    }
+  const elementsToEat = [
+    'ytd-browse[page-subtype="home"]', // Home feed
+    "ytd-shorts", // Shorts feed
+    "ytd-watch-next-secondary-results-renderer", // Watch next feed
+    // 'div#secondary.ytd-watch-flexy', // Watch next feed, alternative selector
+    "div.videowall-endscreen", // End screen recommendations
+    // 'div.html5-endscreen', // End screen recommendations, alternative selector
+  ];
+  for (const selector of elementsToEat) {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach((element) => {
+      element.remove();
+      console.log(`${selector} removed`);
+    });
+  }
 }
 
-feedEater()
+feedEater();
 
-const observer = new MutationObserver(feedEater)
-observer.observe(document.body, { childList: true, subtree: true })
+const observer = new MutationObserver(feedEater);
+observer.observe(document.body, { childList: true, subtree: true });
