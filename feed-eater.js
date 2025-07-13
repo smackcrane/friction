7d982b48da1d5fd99946912f9e2155f2aeccd8a5
Sander Mack-Crane <smackcrane@gmail.com>
2025-07-13 07:08:09 -0600

    eat reddit home/popular feed and top bar recommendations

--- feed-eater.js
+++ feed-eater.js
@@ -1,11 +1,13 @@
 function feedEater() {
   const elementsToEat = [
-    'ytd-browse[page-subtype="home"]', // Home feed
-    "ytd-shorts", // Shorts feed
-    "ytd-watch-next-secondary-results-renderer", // Watch next feed
-    // 'div#secondary.ytd-watch-flexy', // Watch next feed, alternative selector
-    "div.videowall-endscreen", // End screen recommendations
-    // 'div.html5-endscreen', // End screen recommendations, alternative selector
+    "ytd-browse[page-subtype='home']", // youtube home feed
+    "ytd-shorts", // youtube shorts feed
+    "ytd-watch-next-secondary-results-renderer", // youtube watch next feed
+    // 'div#secondary.ytd-watch-flexy', // youtube watch next feed, alternative selector
+    "div.videowall-endscreen", // youtube end screen recommendations
+    // 'div.html5-endscreen', // youtube end screen recommendations, alternative selector
+    "shreddit-feed[reload-url^='/svc/shreddit/feeds/']", // reddit home feed, popular feed
+    "shreddit-gallery-carousel", // reddit top bar recommendations
   ];
   for (const selector of elementsToEat) {
     const elements = document.querySelectorAll(selector);
