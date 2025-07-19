7edcbfbab31a4a9b05c2499e8ee7bd62b9075fa6
Sander Mack-Crane <smackcrane@gmail.com>
2025-07-19 15:56:38 -0600

    branch out feed eater

--- feedEater.js
+++ feedEater.js
@@ -1,7 +1,11 @@
-// For each domain, a list of css selectors for elements to remove, such as
-// - algorithmic feeds and recommendations
-// - login banners
-// - cookie banners
+/**
+ * For each domain, a list of css selectors for elements to remove, such as
+ * - algorithmic feeds and recommendations
+ * - AI search
+ * - AI overview
+ * - login banners
+ * - cookie banners
+ */
 const rolodex = {
   "www.youtube.com": [
     "ytd-browse[page-subtype='home']", // youtube home feed
@@ -10,24 +14,28 @@ const rolodex = {
     // 'div#secondary.ytd-watch-flexy', // youtube watch next feed, alternative selector
     "div.videowall-endscreen", // youtube end screen recommendations
     // 'div.html5-endscreen', // youtube end screen recommendations, alternative selector
+    "ytd-talk-to-recs-flow-renderer.ytdTalkToRecsFlowRendererHost", // youtube AI search
   ],
   "www.reddit.com": [
     "shreddit-feed[reload-url^='/svc/shreddit/feeds/']", // reddit home feed, popular feed
     "shreddit-gallery-carousel", // reddit top bar recommendations
-    "div#credential_picker_container", // "sign in with google" pop-up
   ],
   "www.linkedin.com": [
     "div[data-finite-scroll-hotkey-context='FEED']", // linkedin homepage feed
+    "div#feed-news-module", // linkedin news feed
   ],
-  "www.stackexchange.com": [
-    "div#credential_picker_container", // "sign in with google" pop-up
+  "www.google.com": [
+    "div:has(+div#center_col)", // google search AI overview
   ],
 };
+const universal = [
+  "div#credential_picker_container", // "sign in with google" pop-up
+];
 
 // Get the current domain and its associated selectors
 const url = new URL(window.location.href);
 const domain = url.hostname;
-const selectors = rolodex[domain] ?? [];
+const selectors = universal.concat(rolodex[domain] ?? []);
 console.log(
   "feedEater running on domain:",
   domain,
