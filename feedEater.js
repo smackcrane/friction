c81353b6289394a807e2ee27571c6511b49b64d3
Sander Mack-Crane <smackcrane@gmail.com>
2025-07-23 08:44:43 -0600

    mobile versions of yt, reddit, linkedin

--- feedEater.js
+++ feedEater.js
@@ -1,9 +1,8 @@
 /**
  * For each domain, a list of css selectors for elements to remove, such as
  * - algorithmic feeds and recommendations
- * - AI search
  * - AI overview
- * - login banners
+ * - login / use the app banners
  * - cookie banners
  */
 const rolodex = {
@@ -11,25 +10,37 @@ const rolodex = {
     "ytd-browse[page-subtype='home']", // youtube home feed
     "ytd-shorts", // youtube shorts feed
     "ytd-watch-next-secondary-results-renderer", // youtube watch next feed
-    // 'div#secondary.ytd-watch-flexy', // youtube watch next feed, alternative selector
     "div.videowall-endscreen", // youtube end screen recommendations
-    // 'div.html5-endscreen', // youtube end screen recommendations, alternative selector
-    "ytd-talk-to-recs-flow-renderer.ytdTalkToRecsFlowRendererHost", // youtube AI search
   ],
+  "m.youtube.com": [
+    "div.tab-content[tab-title='Home'][tab-identifier='FEwhat_to_watch']", // youtube mobile home feed
+    "shorts-page", // youtube mobile shorts feed
+    "ytm-single-column-watch-next-results-renderer > ytm-item-section-renderer", // youtube mobile watch next feed
+  ],
+  // www.reddit works for desktop and mobile
   "www.reddit.com": [
     "shreddit-feed[reload-url^='/svc/shreddit/feeds/']", // reddit home feed, popular feed
     "shreddit-gallery-carousel", // reddit top bar recommendations
+    "faceplate-bottom-sheet#xpromo-bottom-sheet", // reddit mobile "View in Reddit app" banner
   ],
+  // www.linkedin works for desktop and mobile, but need different selectors
   "www.linkedin.com": [
-    "div[data-finite-scroll-hotkey-context='FEED']", // linkedin homepage feed
-    "div#feed-news-module", // linkedin news feed
-  ],
-  "www.google.com": [
-    "div:has(+div#center_col)", // google search AI overview
+    "div[data-finite-scroll-hotkey-context='FEED'] ~ div", // divs following linkedin desktop homepage feed, i.e. "Show more feed updates" button
+    "div[data-finite-scroll-hotkey-context='FEED']", // linkedin desktop homepage feed
+    "div#feed-news-module", // linkedin desktop news feed
+    "div.upsell-bottom", // linkedin mobile "Use the LinkedIn app" banner
+    "ol.feed-container > li.new-feed", // linkedin mobile homepage feed
   ],
   "www.facebook.com": [
-    "div.xgmub6v", // facebook top bar reels feed (1 of 3 classes, test to see if it works)
-    "div.x1unhpq9", // facebook home page feed (1 of 3 classes, test to see if it works)
+    "div.xgmub6v", // facebook top bar reels feed (1 of 3 classes, test to see if it works persistently)
+    "div.x1unhpq9", // facebook home page feed (1 of 3 classes, test to see if it works persistently)
+  ],
+  "m.facebook.com": [
+    // there don't seem to be any identifiable css selectors for facebook mobile, so we'll have to find another way
+  ],
+  "ww.google.com": [
+    // ai overview disappeared for me? disable for now, return to it later
+    // "div:has(+div#center_col)", // the thing just before google search results, i.e. AI overview
   ],
 };
 const universal = [
